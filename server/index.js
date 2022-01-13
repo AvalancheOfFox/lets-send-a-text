4dc6e1d42b285f951770fce5b1449f9b699d89d2
Your Name <you@example.com>
2022-01-13 10:32:20 -0600

    refactor

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const pino = require('express-pino-logger')();
+
+const client = require('twilio')(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+)
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get('/api/test', (req,res)=>{
+    const phrase = "This is your phrase";
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ test: `hello, ${phrase}`}))
+})
+
+app.post('/api/messages', (req, res) => {
+    res.header('Content-Type', 'application/json')
+    client.messages.create({
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: req.body.to,
+        body: req.body.body
+    })
+    .then(() => {
+        res.send(JSON.stringify({ success: true}))
+    })
+    .catch(err => {
+        console.warn(err);
+        res.send(JSON.stringify({ success:false }))
+    })
+})
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function buttonOneClick(){
+    console.log("button one was clicked")
+  }
+
+  function buttonTwoClick(){
+    console.log("button two was clicked")
+  }
+
+  function buttonThreeClick(){
+    console.log("button three was clicked")
+  }
+
+  function sendAText(){
+    // If Button One Has Been Clicked &&
+    // If Button Two Has Been Clicked &&
+    // If Button Three Has Been Clicked 
+    // -> 
+    // Then I want to use the Twilio API to send a text
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="buttonRow">
+        <button onClick={buttonOneClick}>One</button>
+        <button onClick={buttonTwoClick}>Two</button>
+        <button onClick={buttonThreeClick}>Three</button>
       </header>
     </div>
   );
